561786c7a5236683881f2d8f65fab585fd81848c
runyud <runyud@umich.edu>
2023-10-02 22:22:32 -0700

    created custom user model and migration

--- articles_api/urls.py
+++ articles_api/urls.py
@@ -1,7 +1,29 @@
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Articles Blog API",
+        default_version="v1",
+        description="API endpoints for Articles Blog API",
+        contact=openapi.Contact(email="runyud@gmail.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0)),
     path(settings.ADMIN_URL, admin.site.urls),
 ]
+
+admin.site.site_header = "Articles Blog API Admin"
+
+admin.site.site_title = "Articles Blog API Admin Portal"
+
+admin.site.index_title = "Welcome to Articles Blog API Portal "
