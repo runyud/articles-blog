6e06097772b67ab3ee3b9985d501e5f894812aad
runyud <runyud@umich.edu>
2023-10-09 17:18:48 -0700

    added celery and redis config

--- articles_api/settings/local.py
+++ articles_api/settings/local.py
@@ -8,3 +8,10 @@ SECRET_KEY = env("DJANGO_SECRET_KEY", default="NHV3xOME7K1gJm7OQ4iwOGuUbzKsxDPP8
 DEBUG = True
 
 CSRF_TRUSTED_ORIGINS = ["http://localhost:8080"]
+
+EMAIL_BACKEND = "djcelery_email.backends.CeleryEmailBackend"
+EMAIL_HOST = env("EMAIL_HOST", default="mailhog")
+EMAIL_PORT = env("EMAIL_PORT")
+DEFAULT_FROM_EMAIL = "support@articlesblog.com"
+DOMAIN = env("DOMAIN")
+SITE_NAME = "Articles Blog"
