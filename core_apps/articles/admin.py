32a4f775aaef48ebcfabae02fa506c3d625a6900
runyud <runyud@umich.edu>
2023-10-19 22:34:23 -0700

    added clap functionality

--- core_apps/articles/admin.py
+++ core_apps/articles/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 from . import models
 
+
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ["pkid", "author", "title", "slug", "view_count"]
     list_display_links = ["pkid", "author"]
@@ -9,13 +10,20 @@ class ArticleAdmin(admin.ModelAdmin):
     search_fields = ["title", "body", "tags"]
     ordering = ["-created_at"]
 
+
 class ArticleViewAdmin(admin.ModelAdmin):
     list_display = ["pkid", "article", "user", "viewer_ip"]
     list_display_links = ["pkid", "article"]
     list_filter = ["created_at", "updated_at"]
     search_fields = ["article", "user", "viewer_ip"]
-    
+
+
+class ClapAdmin(admin.ModelAdmin):
+    list_display = ["pkid", "id", "user", "article"]
+    list_display_links = ["id", "user"]
+    list_filter = ["created_at", "updated_at"]
+
 
 admin.site.register(models.Article, ArticleAdmin)
 admin.site.register(models.ArticleView, ArticleViewAdmin)
-
+admin.site.register(models.Clap, ClapAdmin)
--- core_apps/articles/urls.py
+++ core_apps/articles/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 
-from .views import ArticleListCreateView, ArticleRetrieveUpdateDestroyView
+from .views import (
+    ArticleListCreateView,
+    ArticleRetrieveUpdateDestroyView,
+    ClapArticleView,
+)
 
 urlpatterns = [
     path("", ArticleListCreateView.as_view(), name="article-list-create"),
@@ -8,5 +12,10 @@ urlpatterns = [
         "<uuid:id>/",
         ArticleRetrieveUpdateDestroyView.as_view(),
         name="article-retrieve-update-destroy",
+    ),
+    path(
+        "<uuid:article_id>/clap/",
+        ClapArticleView.as_view(),
+        name="clap-article",
     ),
 ]
